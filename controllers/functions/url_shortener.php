069a56b0b01bbe342a26bf48f6561145b0508ef9
Lionel Paulus <hello@lionelpaul.us>
2016-04-11 21:55:39 +0200

    [NEW] Composer + php-easycache

--- controllers/functions/url_shortener.php
+++ controllers/functions/url_shortener.php
@@ -1,16 +1,27 @@
 <?
+require 'vendor/autoload.php';
+$cache = new iProDev\Util\EasyCache();
+
 function url_shortener($longUrl){
-  $raw = json_encode(array('longUrl' => $longUrl));
+  global $cache;
 
-  $opts = array('http' =>
-      array(
-          'method'  => 'POST',
-          'header'  => 'Content-type: application/json',
-          'content' => $raw
-      )
-  );
+  $result = $cache->get($longUrl);
+  if(!$result) {
+    $raw = json_encode(array('longUrl' => $longUrl));
 
-  $context  = stream_context_create($opts);
-  $result = json_decode(file_get_contents('https://www.googleapis.com/urlshortener/v1/url?key='.GOOGLE_API_KEY, false, $context));
-  return $result->id;
+    $opts = array('http' =>
+        array(
+            'method'  => 'POST',
+            'header'  => 'Content-type: application/json',
+            'content' => $raw
+        )
+    );
+
+    $context  = stream_context_create($opts);
+    $result = json_decode(file_get_contents('https://www.googleapis.com/urlshortener/v1/url?key='.GOOGLE_API_KEY, false, $context));
+    $result = $result->id;
+
+    $cache->set($longUrl, $result);
+  }
+  return $result;
 }
