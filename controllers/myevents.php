ac2ac6688da231fd8b257f1b2d00a73288043e53
LouisRialland <Lo.Rialland@gmail.com>
2016-04-13 11:19:28 +0200

    page my events

---
+++ controllers/myevents.php
@@ -0,0 +1,4 @@
+<?php
+  $title = 'myevents';
+  $class = 'myevents';
+
---
+++ views/pages/myevents.php
@@ -0,0 +1,66 @@
+<div class="background_event">
+  <div class="content_event">
+  	<div class='content_image'>
+  	</div>
+    <div class='content_text'>
+  		<div class='title'>Batman Versus Superman</div>
+  		<div class='date'>10/09 at 9:00 PM</div>
+  		 <div class='organisator'> Organisateur : Louis Rialland</div>
+	<div class="fab blue">
+	  <i class="material-icons md-48 icons_logo">perm_identity</i>
+      
+      <paper-ripple class="circle recenteringTouch" fit>
+      </paper-ripple>
+    </div>
+   
+  	</div>
+ </div>
+
+ <div class="content_event">
+  	<div class='content_image'>
+  	</div>
+    <div class='content_text'>
+  		<div class='title'>Batman Versus Superman</div>
+  		<div class='date'>10/09 at 9:00 PM</div>
+	<div class="fab green">
+	  <i class="material-icons md-48 icons_logo">done</i>
+      
+      <paper-ripple class="circle recenteringTouch" fit>
+      </paper-ripple>
+    </div>
+    <div class='organisator'> Organisateur : Louis Rialland</div>
+  	</div>
+ </div>
+
+ <div class="content_event">
+  	<div class='content_image'>
+  	</div>
+    <div class='content_text'>
+  		<div class='title'>Batman Versus Superman</div>
+  		<div class='date'>10/09 at 9:00 PM</div>
+	<div class="fab red">
+	  <i class="material-icons icons_logo">close</i>
+      
+      <paper-ripple class="circle recenteringTouch" fit>
+      </paper-ripple>
+    </div>
+    <div class='organisator'> Organisateur : Louis Rialland</div>
+  	</div>
+ </div>
+
+ <div class="content_event">
+  	<div class='content_image'>
+  	</div>
+    <div class='content_text'>
+  		<div class='title'>Batman Versus Superman</div>
+  		<div class='date'>10/09 at 9:00 PM</div>
+	<div class="fab yellow">
+	  <i class="material-icons icons_logo_alert">notifications_active</i>
+      
+      <paper-ripple class="circle recenteringTouch" fit>
+      </paper-ripple>
+    </div>
+    <div class='organisator'> Organisateur : Louis Rialland</div>
+  	</div>
+ </div>
+</div>
