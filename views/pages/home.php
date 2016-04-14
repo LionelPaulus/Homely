adc8193c2df0ddd2460823947119c11002d70e36
Lionel Paulus <hello@lionelpaul.us>
2016-04-15 00:32:24 +0200

    [FIX] Grammatical fault

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,7 +1,7 @@
 <div class="background">
   <div class="content">
     <h1>Homely</h1>
-    <h2>Organise easily your cinema sessions with friends and at home</h2>
+    <h2>Organize easily your cinema sessions with friends and at home</h2>
     <a class="mui-btn mui-btn--primary mui-btn--raised" href="<?= $loginUrl ?>">FACEBOOK CONNECT</a>
   </div>
 </div>
