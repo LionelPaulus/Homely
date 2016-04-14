73a166d11c13b8850fef601632527c25ecb73b68
LouisRialland <Lo.Rialland@gmail.com>
2016-04-14 16:52:01 +0200

    Pages infos Films V2

--- views/pages/info-films.php
+++ views/pages/info-films.php
@@ -1,32 +1,43 @@
  
  <div class="mui-container container_total">
- 	<div class='mui-container-fluid'>  	 
- 	<img class='background_image'src='src/images/glad.jpg'/>
+ 	<div class='mui-container-fluid container_background'>  	 
+ 		<img class='background_image'src='src/images/glad.jpg'/>
+	 	<div class='wrapper_wrapper'>
+	 	<div class='mui--pull-right wrapper_overlay'>
+	 			<div class='background_overlay'></div>
+				<div class='text_overlay'>
+			 		<div class="mui--text-subhead ">1995</div>
+			 		<div class="mui--text-title title "><strong>Gladiator</strong> </div>
+			 		<div class="mui--text-subhead ">9.8/10</div>
+		 		</div>
+	 	</div>
+	 	</div>
  	</div>
  	
  	<div class='mui-container writing'>
- 		<div class='background_overlay'></div>
- 		<div class="mui-container-fluid overlay">
- 		<div class="mui--text-title ">1995</div>
- 		<div class="mui--text-display1 title "><strong>Gladiator</strong> </div>
- 		<div class="mui--text-title ">9.8/10</div>
+ 		
+ 		
+ 		
+ 		<div class="mui--container-fluid container_poster">  
+ 			<img class='poster_image'src='src/images/poster.jpg' />
  		</div>
- <img class='poster_image'src='src/images/poster.jpg' />
-		<div class='mui-container important_info'>
+ 		<div class='mui-container-fluid important_info'>
  		
 		
 			
-		 	<div class='mui--text-title '> 2H45 min</div>
-		 	<div class='mui--text-title '> Genre : Action,Mythologie</div>
-		 	<div class='mui--text-title '> Director : G. Lucas</div>
+		 	<div class=' '> 2H45 min</div>
+		 	<div class=' '> Genre : Action,Mythologie</div>
+		 	<div class=' '> Director : G. Lucas</div>
 		</div>
+ 		  
+	
 		<div class="mui--z2 container_synopsis">
 			<div class="mui--text-subhead  subtitle"><strong>About Movie</strong></div>
 			<div class="mui-divider"></div>
-			<div class="mui--text-body2 synopsis">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et expedita neque iusto amet necessitatibus labore minus beatae quasi quia sit eveniet, quo nisi omnis, in eligendi distinctio dolore fuga harum!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore consequuntur sint consectetur atque, asperiores laborum molestiae deserunt odio commodi! Provident, velit. Minus quod provident omnis debitis iste ad molestiae consectetur.Lorem DEHFSDFKSDPdk dlfskdifsufsdfsfs deibfsdbfkdpfj sdbcib</div>
+			<div class="mui--text-body1 synopsis">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et expedita neque iusto amet necessitatibus labore minus beatae quasi quia sit eveniet, quo nisi omnis, in eligendi distinctio dolore fuga harum!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore consequuntur sint consectetur atque, asperiores laborum molestiae deserunt odio commodi! Provident, velit. Minus quod provident omnis debitis iste ad molestiae consectetur.Lorem DEHFSDFKSDPdk dlfskdifsufsdfsfs deibfsdbfkdpfj sdbcib</div>
 			<div class="mui--text-subhead  subtitle"><strong>Movie Details</strong></div>
 			<div class="mui-divider"></div>
-			<div class="mui--text-body2 synopsis">Directed : Zhony Bryan</br>Cast : Ben Afflek, Joujou Coligo, Derrek Shepard</div>
+			<div class="mui--text-body1 synopsis">Directed : Zhony Bryan</br>Cast : Ben Afflek, Joujou Coligo, Derrek Shepard</div>
 			
 
 		</div>
