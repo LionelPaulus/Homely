f85e6f7f90c28a8e7b3f00504204dedb60442c48
LouisRialland <Lo.Rialland@gmail.com>
2016-04-14 22:57:30 +0200

    info film V3

--- views/pages/info-films.php
+++ views/pages/info-films.php
@@ -1,7 +1,9 @@
  
  <div class="mui-container container_total">
- 	<div class='mui-container-fluid container_background'>  	 
+ 	<div class='mui-container-fluid container_background'>  
+ 		 
  		<img class='background_image'src='src/images/glad.jpg'/>
+ 		</div>
 	 	<div class='wrapper_wrapper'>
 	 	<div class='mui--pull-right wrapper_overlay'>
 	 			<div class='background_overlay'></div>
