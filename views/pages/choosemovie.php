2d0c1685c1486c81c11b61fcf78cd1df9eb8517a
sulivann <nguyen.sulivan@gmail.com>
2016-04-15 14:33:57 +0200

    Remove choose movie

--- views/pages/choosemovie.php
+++
@@ -1,8 +0,0 @@
-<form action="#" method="POST">
-	<input type="search" name="movie" class="movieName">
-	<button>Search</button>
-</form>
-<div class="selected">
-	<h2>Vos films sélectionnés</h2>
-</div>
-
